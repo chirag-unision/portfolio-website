2ffdaabd614a6f2deae6da9b561d1e08c383ca26
chirag-unision <98763725+chirag-unision@users.noreply.github.com>
2022-01-31 23:51:40 +0530

    Add files via upload

---
+++ blogs.js
@@ -0,0 +1,82 @@
+const data= [
+    {
+        "title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem est nostrum reprehenderit rerum cum.",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet delectus accusantium voluptas adipisci. Facilis amet odio cupiditate quo nemo laudantium ut corrupti laborum facere ipsa, alias, vel temporibus velit perferendis!",
+        "timeofpost": "30/01/22 | 02:30 P.M. ",
+        "postedby": "Chirag Kaushik (Admin)"
+    },
+    {
+        "title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem est nostrum reprehenderit rerum cum.",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet delectus accusantium voluptas adipisci. Facilis amet odio cupiditate quo nemo laudantium ut corrupti laborum facere ipsa, alias, vel temporibus velit perferendis!",
+        "timeofpost": "30/01/22 | 02:30 P.M. ",
+        "postedby": "Chirag Kaushik (Admin)"
+    },
+    {
+        "title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem est nostrum reprehenderit rerum cum.",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet delectus accusantium voluptas adipisci. Facilis amet odio cupiditate quo nemo laudantium ut corrupti laborum facere ipsa, alias, vel temporibus velit perferendis!",
+        "timeofpost": "30/01/22 | 02:30 P.M. ",
+        "postedby": "Chirag Kaushik (Admin)"
+    },
+    {
+        "title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem est nostrum reprehenderit rerum cum.",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet delectus accusantium voluptas adipisci. Facilis amet odio cupiditate quo nemo laudantium ut corrupti laborum facere ipsa, alias, vel temporibus velit perferendis!",
+        "timeofpost": "30/01/22 | 02:30 P.M. ",
+        "postedby": "Chirag Kaushik (Admin)"
+    },    {
+        "title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem est nostrum reprehenderit rerum cum.",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet delectus accusantium voluptas adipisci. Facilis amet odio cupiditate quo nemo laudantium ut corrupti laborum facere ipsa, alias, vel temporibus velit perferendis!",
+        "timeofpost": "30/01/22 | 02:30 P.M. ",
+        "postedby": "Chirag Kaushik (Admin)"
+    },    {
+        "title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem est nostrum reprehenderit rerum cum.",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet delectus accusantium voluptas adipisci. Facilis amet odio cupiditate quo nemo laudantium ut corrupti laborum facere ipsa, alias, vel temporibus velit perferendis!",
+        "timeofpost": "30/01/22 | 02:30 P.M. ",
+        "postedby": "Chirag Kaushik (Admin)"
+    },    {
+        "title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem est nostrum reprehenderit rerum cum.",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet delectus accusantium voluptas adipisci. Facilis amet odio cupiditate quo nemo laudantium ut corrupti laborum facere ipsa, alias, vel temporibus velit perferendis!",
+        "timeofpost": "30/01/22 | 02:30 P.M. ",
+        "postedby": "Chirag Kaushik (Admin)"
+    },    {
+        "title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem est nostrum reprehenderit rerum cum.",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet delectus accusantium voluptas adipisci. Facilis amet odio cupiditate quo nemo laudantium ut corrupti laborum facere ipsa, alias, vel temporibus velit perferendis!",
+        "timeofpost": "30/01/22 | 02:30 P.M. ",
+        "postedby": "Chirag Kaushik (Admin)"
+    },
+    {
+        "title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem est nostrum reprehenderit rerum cum.",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet delectus accusantium voluptas adipisci. Facilis amet odio cupiditate quo nemo laudantium ut corrupti laborum facere ipsa, alias, vel temporibus velit perferendis!",
+        "timeofpost": "30/01/22 | 02:30 P.M. ",
+        "postedby": "Chirag Kaushik (Admin)"
+    },
+    {
+        "title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem est nostrum reprehenderit rerum cum.",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet delectus accusantium voluptas adipisci. Facilis amet odio cupiditate quo nemo laudantium ut corrupti laborum facere ipsa, alias, vel temporibus velit perferendis!",
+        "timeofpost": "30/01/22 | 02:30 P.M. ",
+        "postedby": "Chirag Kaushik (Admin)"
+    },
+    {
+        "title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem est nostrum reprehenderit rerum cum.",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet delectus accusantium voluptas adipisci. Facilis amet odio cupiditate quo nemo laudantium ut corrupti laborum facere ipsa, alias, vel temporibus velit perferendis!",
+        "timeofpost": "30/01/22 | 02:30 P.M. ",
+        "postedby": "Chirag Kaushik (Admin)"
+    },
+    {
+        "title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem est nostrum reprehenderit rerum cum.",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet delectus accusantium voluptas adipisci. Facilis amet odio cupiditate quo nemo laudantium ut corrupti laborum facere ipsa, alias, vel temporibus velit perferendis!",
+        "timeofpost": "30/01/22 | 02:30 P.M. ",
+        "postedby": "Chirag Kaushik (Admin)"
+    },
+    {
+        "title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem est nostrum reprehenderit rerum cum.",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet delectus accusantium voluptas adipisci. Facilis amet odio cupiditate quo nemo laudantium ut corrupti laborum facere ipsa, alias, vel temporibus velit perferendis!",
+        "timeofpost": "30/01/22 | 02:30 P.M. ",
+        "postedby": "Chirag Kaushik (Admin)"
+    },
+    {
+        "title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem est nostrum reprehenderit rerum cum.",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet delectus accusantium voluptas adipisci. Facilis amet odio cupiditate quo nemo laudantium ut corrupti laborum facere ipsa, alias, vel temporibus velit perferendis!",
+        "timeofpost": "30/01/22 | 02:30 P.M. ",
+        "postedby": "Chirag Kaushik (Admin)"
+    }
+]
---
+++ quiz.js
@@ -0,0 +1,82 @@
+const data= [
+    {
+        "question": "What is React.js?",
+        "optionA": "programming language",
+        "optionB": "framework",
+        "optionC": "library",
+        "optionD": "none of these",
+        "answer": "C"
+    },
+    {
+        "question": "What is the full form of CSRF?",
+        "optionA": "Cross Site Request Forgery",
+        "optionB": "Common Site Resourse Forum",
+        "optionC": "Critical Site Request Forgery",
+        "optionD": "none of these",
+        "answer": "A"
+    },
+    {
+        "question": "What CSS property can we use to rotate a DOM element?",
+        "optionA": "filter",
+        "optionB": "transform",
+        "optionC": "transition",
+        "optionD": "none of these",
+        "answer": "B"
+    },
+    {
+        "question": "Which of the following is not a javascript inbuilt object?",
+        "optionA": "window",
+        "optionB": "document",
+        "optionC": "date",
+        "optionD": "all are inbuilt objects",
+        "answer": "D"
+    },
+    {
+        "question": "What of the following is not a right way to deine a variable in javascript?",
+        "optionA": "var data= 10;",
+        "optionB": "let data= 10;",
+        "optionC": "const data= 10;",
+        "optionD": "all are correct",
+        "answer": "C"
+    },
+    {
+        "question": "Which of the following is not a loop?",
+        "optionA": "goto",
+        "optionB": "while",
+        "optionC": "for in",
+        "optionD": "for of",
+        "answer": "A"
+    },
+    {
+        "question": "Select the right PHP syntax?",
+        "optionA": "$variable= 10",
+        "optionB": "var variable= 10",
+        "optionC": "variable= 10",
+        "optionD": "none of these",
+        "answer": "A"
+    },
+    {
+        "question": "Which of the following is incorrect?",
+        "optionA": "PHP is a server-side scripting language",
+        "optionB": "PHP is a client-side scripting language",
+        "optionC": "CodeIgnitor is a framework of PHP ",
+        "optionD": "Laravel is an MVC framework",
+        "answer": "B"
+    },
+    {
+        "question": "What is not a scripting language?",
+        "optionA": "PHP",
+        "optionB": "Javascript",
+        "optionC": "Python",
+        "optionD": "CSS",
+        "answer": "D"
+    },
+    {
+        "question": "Which is not a javascript web api?",
+        "optionA": "fetch()",
+        "optionB": "findIndex()",
+        "optionC": "map()",
+        "optionD": "getJSON()",
+        "answer": "D"
+    }
+]
